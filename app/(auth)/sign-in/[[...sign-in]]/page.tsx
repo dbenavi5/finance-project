82775f5465fd77c795eab1c938084e1314db3bf7
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2024-05-23 01:08:23 -0700

    header + navigation ui, in mobile + desktop  view

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
           </ClerkLoading>
         </div>
       </div>
-      <div className="h-full bg-blue-500 hidden lg:flex items-center justify-center">
+      <div className="h-full bg-teal-500 hidden lg:flex items-center justify-center">
         <Image src="/logo.svg" height={100} width={100} alt="Logo" />
       </div>
     </div>
